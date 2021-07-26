726e089e8b605d5e3e3c67fe1d8dff37877778ad
永井光陽 <k_nagai@AN-MGN73JS04.local>
2021-07-26 11:22:31 +0900

    create top page

---
+++ GitClass/Top/TopViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TopViewController.swift
+//  GitClass
+//
+//  Created by 永井光陽 on 2021/07/26.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+}
+
+// MARK: - method
+extension TopViewController {
+}
+
